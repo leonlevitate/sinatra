bac186f31874566d37e6ac9c025e605b51defe50
leon.linton@live.co.uk <leon.linton@live.co.uk>
2019-06-11 15:43:13 +0100

    Added POST requests

--- app.rb
+++ app.rb
@@ -6,11 +6,15 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
+end
+
+get '/form-cat' do
+  erb :catindex
 end
