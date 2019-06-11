400a87738938737409a00f18e162a4bca79ee05e
leon.linton@live.co.uk <leon.linton@live.co.uk>
2019-06-11 14:23:37 +0100

    Add parameters

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ get '/' do
   "Hello World!!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
